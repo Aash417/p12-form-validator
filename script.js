2bbf114774ee0bba18d0b48b6d2b4c8851391ab1
aashish_k <aashishkathat00@gmail.com>
2023-09-06 21:41:58 +0530

    form validation with js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const form = document.getElementById('form');
-const passwrod1 = document.getElementById('passwrod1');
+const password1 = document.getElementById('password1');
 const password2 = document.getElementById('password2');
 const messageContainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
@@ -14,29 +14,47 @@ function validateForm() {
     message.textContent = 'Please fill out all fields';
     message.style.color = 'red';
     messageContainer.style.borderColor = 'red';
+    return;
   }
   // check if password match
 
-  if (passwrod1.value === password2.value) {
+  if (password1.value === password2.value) {
     passwordMatch = true;
-    passwrod1.style.borderColor = 'green';
+    password1.style.borderColor = 'green';
     password2.style.borderColor = 'green';
   } else {
     passwordMatch = false;
     message.textContent = 'Make sure password match';
     message.style.color = 'red';
     messageContainer.style.borderColor = 'red';
-    passwrod1.style.borderColor = 'red';
+    password1.style.borderColor = 'red';
     password2.style.borderColor = 'red';
+    return;
+  }
+  // if form is valid
+  if (isValid && passwordMatch) {
+    message.textContent = 'Successfully Registered';
+    message.style.color = 'green';
+    messageContainer.style.borderColor = 'green';
   }
 }
-
+function storeFormData() {
+  const user = {
+    name: form.name.value,
+    phone: form.phone.value,
+    email: form.email.value,
+    website: form.url.value,
+    password: form.password.value,
+  };
+  console.log(user);
+}
 function processFormData(e) {
   e.preventDefault();
 
   //   validate form
   validateForm();
+  //   if valid
+  if (isValid && passwordMatch) storeFormData();
 }
-
 // event listner
 form.addEventListener('submit', processFormData);
